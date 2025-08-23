b58ec793650ef28185524050e7ed8d6d3d3be255
Edinaelson Galeno <sedinaelson@gmail.com>
2025-08-23 11:34:19 -0300

    test: add unit tests for Text service

---
+++ Moderation/Services/Text.cs
@@ -0,0 +1,19 @@
+﻿namespace Moderation.Services;
+
+public class Text
+{
+    public int Input(string input)
+    {
+        List<string> wordsToFind = new List<string>{"bater", "chutar", "atirar", "morder", "arranhar", "furar", "ameçar", "enterrar"};
+        int occurrence = 0;
+        foreach (var word in wordsToFind)
+        {
+            if (input.Contains(word, StringComparison.OrdinalIgnoreCase))
+            {
+                occurrence++;
+            }
+        }
+        
+        return occurrence;
+    }
+}
