ba8be54d473a6ae3a45e9f40c74bbfe66b42fb98
Edinaelson Galeno <sedinaelson@gmail.com>
2025-08-26 15:50:51 -0300

    fix: bug of git

--- ModerationTest/Services/ModerationIATest.cs
+++ ModerationTest/Services/ModerationIATest.cs
@@ -1,9 +1,6 @@
-﻿using System.ClientModel;
-using System.ClientModel.Primitives;
-using Moderation.DTO;
+﻿using Moderation.DTO;
 using Moderation.Interfaces;
 using Moq;
-using OpenAI.Moderations;
 
 namespace Moderation.Services;
 
@@ -11,31 +8,19 @@ namespace Moderation.Services;
 public class ModerationIATest
 {
     [Test]
-    public void Moderate_ShoudDetectViolenceWhenPresent()
+    public async Task Moderate_ShoudDetectViolenceWhenPresent()
     {
         var mockClient = new Mock<IModerationClient>();
         
-        var fakeResponse = new ModerationResultDto(
-            Flagged: true,
-            Violence: true,
-            SelfHarm: false,
-            Sexual: false,
-            Hate: false
-        );
-        
-        mockClient.Setup(c => c.ClassifyText(It.IsAny<string>()))
-            .Returns(fakeResponse);
+        mockClient.Setup(c => c.IsIllicitOrViolentAsync(It.IsAny<string>()))
+            .ReturnsAsync(true);
         
         var service = new ModerationIA(mockClient.Object);
         
-        var result = service.Moderate("I want to kill them.");
+        var result =  await service.Moderate("quero matar pessoas");
         
-        Assert.IsTrue(result.Flagged);
-        Assert.IsTrue(result.Violence);
-        Assert.IsFalse(result.SelfHarm);
-        Assert.IsFalse(result.Sexual);
-        Assert.IsFalse(result.Hate);
-
+        Assert.IsTrue(result);
+        
     }
     
 }
